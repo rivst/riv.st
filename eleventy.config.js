59da3a5c141cc5290851cb52cfe10a37a70c2ab0
alex <me@riv.st>
2025-06-16 17:56:40 +0300

    feat: Add notes collection

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,4 +2,16 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("assets/fonts");
   eleventyConfig.addPassthroughCopy("now.txt");
+  eleventyConfig.addFilter("post_date", (dateObj) => {
+    return `${dateObj.toLocaleString("default", {
+      dateStyle: "medium",
+      timeStyle: "short",
+    })}`;
+  });
+  eleventyConfig.addFilter("exclude", (collection, stringToFilter) => {
+    if (!stringToFilter) {
+      return collection;
+    }
+    return (collection ?? []).filter((item) => item !== stringToFilter);
+  });
 }
---
+++ notes/notes.11tydata.js
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+export default function () {
+  return {
+    layout: "layouts/note.njk",
+    tags: ["note"],
+    eleventyComputed: {
+      lastModified: (data) => {
+        const stats = fs.statSync(data.page.inputPath);
+        return stats.mtime;
+      },
+    },
+  };
+}
